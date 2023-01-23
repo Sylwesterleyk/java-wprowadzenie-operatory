7db5a653d8252b83c02e86020c4b401b3e4a136c
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-23 18:28:41 +0100

    #6 Dodanie plików programu opisującego moją ostatnią wycieczkę

---
+++ App3.java
@@ -0,0 +1,15 @@
+class App3 {
+    public static void main(String args[]){
+        double kilometers = 1542.6;
+        int minutes = 13108;
+        byte days = 9;
+        short accountBalance = 8698;
+
+        System.out.println("Witajcie, oto krótka historia mojej ostaniej podróży szlakiem Beskidu Wyspowego.");
+        System.out.println("Eskapadę rozpocząłem 4 maja 2023 roku mając do dyspozycji środki w kwocie "+accountBalance+" złotych.");
+        System.out.println("Przebyłem samochodem oraz pieszo łącznie "+kilometers+" kilometry.");
+        System.out.println("Zajęło mi to "+days+" dni, a będąc precyzyjnym to "+minutes+" minuty.");
+        System.out.println("Po zakończonej wyprawie stan mojego konta wynosił"+(accountBalance-3233));
+
+    }
+}
