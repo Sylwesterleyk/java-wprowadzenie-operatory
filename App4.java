5f26fbd992a7bcc176bc6aded89ad285e1678bf0
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-23 22:33:00 +0100

    #8 Dodanie programu sumującego dwie liczby całkowite

---
+++ App4.java
@@ -0,0 +1,11 @@
+class App4 {
+    public static void main(String args[]){
+        byte number1 = 89;
+        int number2= 200;
+
+        System.out.println("Liczba pierwsza: "+number1);
+        System.out.println("Liczba druga: "+number2);
+        System.out.println("Wynik sumowanie liczby pierwszej i drugiej: "+(number1+number2));
+
+    }
+}
