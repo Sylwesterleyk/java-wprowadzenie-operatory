539e4dab8a99ba84276be7c3c2e7bcb9d6d49eb5
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-23 23:06:05 +0100

    #11 Dodanie programu obliczającego obwód trójkąta

---
+++ App5.java
@@ -0,0 +1,12 @@
+class App5 {
+    public static void main(String args[]){
+        int a=7;
+        int b=9;
+        int c=12;
+        int trianglePerimeter= (a+b+c);
+
+        System.out.println("Długości boków trójkąta to odpowiednio: "+a+","+b+","+c);
+        System.out.println("Obwód tego trójkąta to: "+trianglePerimeter);
+
+    }
+}
