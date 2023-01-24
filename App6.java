15dae6f30ee128013ea0b5cb6814ef8a25f91384
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-24 11:12:35 +0100

    wprowadzenie zmian do App6

--- App6.java
+++ App6.java
@@ -1,12 +1,11 @@
 class App6 {
     public static void main(String args[]){
-        int x = -676;
-        int y = 676;
-        int z = -676;
+        int x = 676;
+        int wartoscAbsolutna;
 
-       int isSmaller = (x<0)? z:y;
+       wartoscAbsolutna = x >=0 ? x : -x;
 
-        System.out.println("Jeśli liczba x wynosi |"+x+"|,to jej liczbą bezwzględną jest "+isSmaller);
+        System.out.println("Jeśli liczba x wynosi |"+x+"|,to jej liczbą bezwzględną jest "+wartoscAbsolutna);
 
     }
 }
